3bd8bfcdd8fea24705292c4fbcefa4a88006f5dc
alberthgrande <alberthgrande98@gmail.com>
2026-04-10 18:20:27 +0800

    modified middlewares"

--- RBACv1/src/middlewares/auth.middleware.js
+++ RBACv1/src/middlewares/auth.middleware.js
@@ -1,9 +1,29 @@
+// import jwt from "jsonwebtoken";
+
+// export const authenticate = (req, res, next) => {
+//   try {
+//     const token = req.headers.authorization?.split(" ")[1];
+//     if (!token) res.status(401).json({ message: "No token provided" });
+
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+//     req.user = decoded;
+
+//     next();
+//   } catch (error) {
+//     res.status(401).json({ message: "Invalid token", error });
+//   }
+// };
+
 import jwt from "jsonwebtoken";
 
 export const authenticate = (req, res, next) => {
   try {
     const token = req.headers.authorization?.split(" ")[1];
-    if (!token) res.status(401).json({ message: "No token provided" });
+
+    if (!token) {
+      return res.status(401).json({ message: "No token provided" });
+    }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
@@ -11,6 +31,9 @@ export const authenticate = (req, res, next) => {
 
     next();
   } catch (error) {
-    res.status(401).json({ message: "Invalid token", error });
+    return res.status(401).json({
+      message: "Invalid token",
+      error: error.message,
+    });
   }
 };
