a7e3bc0cf6bf5a1572f5bbebc5404a1632692659
alberthgrande <alberthgrande98@gmail.com>
2026-04-10 12:38:23 +0800

    create a database connection

--- RBACv1/src/config/db.js
+++ RBACv1/src/config/db.js
@@ -1,5 +1,21 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-
 import { Pool } from "pg";
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+async function testConnection() {
+  try {
+    const res = await pool.query("SELECT 1");
+    console.log("Connected successfully:", res.rows);
+  } catch (err) {
+    console.error("Connection error:", err);
+  } finally {
+    await pool.end();
+  }
+}
+
+testConnection();
